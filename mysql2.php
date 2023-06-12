a21fb8aaca56873ea52345a6914843cec1c333f7
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-06-12 21:38:48 +0600

    add value in my sql from php and hash password

---
+++ mysql2.php
@@ -0,0 +1,20 @@
+<?php
+  include("database2.php");
+
+  $username = "Patrick";
+  $password = "rock3";
+  $hash = password_hash($password, PASSWORD_DEFAULT);
+
+  $sql = "INSERT INTO users (user, password)
+          VALUES ('$username', '$hash')";
+
+  try{
+     mysqli_query($conn, $sql);
+     echo"User is now register";
+  }
+  catch(mysqli_sql_exception){
+    echo"Could not register user";
+  }
+
+  mysqli_close($conn);
+?>
